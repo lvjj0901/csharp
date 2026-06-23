f31e03f38645ba96e89bb0e5840377c13cc322b2
JasonLyu <lvjj0901@gmail.com>
2026-06-22 22:53:02 -0400

    class , exception and encapsulation

---
+++ Module05_Session02/Records_ Equality_Immutability/Indexers.cs
@@ -0,0 +1,28 @@
+﻿//Week week = new Week();
+//Console.WriteLine(week[0]);
+public class Week 
+{ 
+    private readonly string[] days = new string[] 
+                                    { 
+                                        "Monday", 
+                                        "Tuesday", 
+                                        "Wednesday", 
+                                        "Thursday", 
+                                        "Friday", 
+                                        "Saturday", 
+                                        "Sunday" 
+                                    };
+
+
+    public string this[int index] 
+    {
+        get
+        {
+            if (index < 0 || index >= this.days.Length)
+            {
+                throw new IndexOutOfRangeException($"Index must be between 0 and {this.days.Length - 1}.");
+            }
+            return this.days[index];
+        }
+    }
+}
---
+++ Module05_Session02/Records_ Equality_Immutability/Program.cs
@@ -0,0 +1,61 @@
+﻿var account = new Account("10001","Jason",500m);
+//var account1 = new Account("10002", "Mike");
+account.Deposit(100m);
+Console.WriteLine($"account Balance: {account.Balance}");
+account.Withdraw(50m);
+Console.WriteLine($"account Balance: {account.Balance}");
+
+public class Account
+{
+    public string AccountNumber { get; init; }
+
+    public string Owner { get; init; }
+
+    public decimal Balance { get; private set; }
+
+    public Account(string accountNumber, string owner) : this(accountNumber, owner, 0m)
+    {
+        this.AccountNumber = accountNumber;
+        this.Owner = owner;
+        this.Balance = 0m;
+    }
+
+    public Account(string accountNumber, string owner,decimal openingBalance)
+    {
+        this.AccountNumber = accountNumber;
+        this.Owner = owner;
+        if (openingBalance < 0)
+        { 
+            throw new ArgumentException("Opening balance cannot be negative.");
+        }
+        this.Balance = openingBalance;
+    }  
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException(
+                "Deposit amount must be positive.");
+        }
+
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException(
+                "Withdrawal amount must be positive.");
+        }
+
+        if (amount > Balance)
+        {
+            throw new InvalidOperationException(
+                "Insufficient funds.");
+        }
+
+        Balance -= amount;
+    }
+}
